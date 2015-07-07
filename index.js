f23d8ada09029465b9db30a9f779b70cfb9c8ae0
Stefan Walther <swr@qlik.com>
2015-07-07 19:47:02 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * shared-styles <https://github.com/stefanwalther/shared-styles>
+ *
+ * Copyright (c) 2015, Stefan Walther.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
